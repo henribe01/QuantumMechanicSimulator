Fixed small error in calculation for kinetic operator

main.py
@@ -21,22 +21,28 @@ def gaussian_wave_packet(spatial_grid: np.ndarray, x_0: float, k_0: float,
 
 
 def infinite_potential_well_eigenfunction(spatial_grid: np.ndarray,
-                                          n: int) -> np.ndarray:
-    L = x_max - x_min
-    return np.sqrt(2 / L) * np.sin(n * np.pi * (spatial_grid - x_min) / L)
+                                          n: int, left_wall: float,
+                                          right_wall: float) -> np.ndarray:
+    L = right_wall - left_wall
+    inside_well = np.logical_and(spatial_grid >= left_wall,
+                                 spatial_grid <= right_wall)
+    eigenfunction = np.zeros(len(spatial_grid))
+    eigenfunction[inside_well] = np.sqrt(2 / L) * np.sin(
+        n * np.pi / L * (spatial_grid[inside_well] - left_wall))
+    return eigenfunction
 
 
 def ground_state_harmonic(spatial_grid: np.ndarray, x_0: float,
                           omega: float) -> np.ndarray:
-    return np.sqrt(np.sqrt(mass * omega / (hbar * np.pi))) * np.exp(
-        -0.5 * mass * omega / hbar * np.square(spatial_grid - x_0))
+    return np.power(mass * omega / (np.pi * hbar), 0.25) * np.exp(
+        -mass * omega * np.square(spatial_grid - x_0) / (2 * hbar))
 
 
 if __name__ == '__main__':
     x_min = 0
     x_max = 15
     center = (x_max - x_min) / 2
-    k_0 = 0
+    k_0 = 5
     mass = 1
     hbar = 1
     # Should be smaller than x_max - x_min and larger than 0.05
@@ -47,14 +53,16 @@ if __name__ == '__main__':
     spatial_grid = np.linspace(x_min, x_max, 2000)
 
     omega = 0.5
-    particle = Particle(spatial_grid, gaussian_wave_packet, x_0=center, k_0=0,
-                        width=width)
-    # particle = Particle(spatial_grid, ground_state_harmonic, x_0=center,
-    #                    omega=omega)
-    potential = InfiniteSquareWell(spatial_grid, 2, 13)
-    #potential = HarmonicOscillator(spatial_grid, center, omega)
+    # particle = Particle(spatial_grid, gaussian_wave_packet, x_0=center, k_0=0,
+    #                    width=width)
+    particle = Particle(spatial_grid, ground_state_harmonic, x_0=center,
+                        omega=omega)
+    # particle = Particle(spatial_grid, infinite_potential_well_eigenfunction,
+    #                    n=5, left_wall=2, right_wall=13)
+    # potential = InfiniteSquareWell(spatial_grid, 2, 13)
+    potential = HarmonicOscillator(spatial_grid, center, omega, mass)
     dt = 0.001
-    steps = 2000
+    steps = 5000
 
     # Plot
     fig, ax = plt.subplots()
@@ -63,31 +71,36 @@ if __name__ == '__main__':
     real_line, = ax.plot([], [], label="Re")
     imag_line, = ax.plot([], [], label="Im")
     abs_line, = ax.plot([], [], label="Abs")
+    prob_line, = ax.plot([], [], label="Prob")
     ax.legend()
     potential_line = potential.plot(ax, facecolor='gray', edgecolor='black')
     ax.fill_between(particle.spatial_grid, -1, facecolor='gray',
                     edgecolor='black')
 
     result = np.zeros((steps, len(particle.spatial_grid)), dtype=complex)
+    probs = np.zeros((steps, len(particle.spatial_grid)))
     start = timeit.default_timer()
     for i in range(steps):
         particle.simulation_step(potential.get_potential(), dt)
         result[i] = particle.psi
+        probs[i] = particle.get_probability_density()
     stop = timeit.default_timer()
     print('Time: ', stop - start)
 
     # Animation
-    speed = 2
+    speed = 20
 
 
     def animate(i):
         real_line.set_data(particle.spatial_grid, np.real(result[i * speed]))
         imag_line.set_data(particle.spatial_grid, np.imag(result[i * speed]))
         abs_line.set_data(particle.spatial_grid, np.abs(result[i * speed]))
-        return real_line, imag_line, abs_line,
+        prob_line.set_data(particle.spatial_grid, probs[i * speed])
+        return real_line, imag_line, abs_line, prob_line
 
 
-    anim = FuncAnimation(fig, animate, frames=steps, interval=1/60,
+    anim = FuncAnimation(fig, animate, frames=steps // speed,
+                         interval=1 / 60,
                          repeat=True)
     # print(particle.psi)
     plt.show()

particle.py
@@ -40,8 +40,8 @@ class Particle:
         """
         off_diag = np.ones(len(self.spatial_grid) - 1)
         diag = -2 * np.ones(len(self.spatial_grid))
-        return sp.sparse.diags([off_diag, diag, off_diag],
-                               [-1, 0, 1], format="csr") / self.dx ** 2
+        return -sp.sparse.diags([off_diag, diag, off_diag],
+                               [-1, 0, 1], format="csr") / (2 * self.dx ** 2)
 
     def simulation_step(self, potential: sp.sparse.csr_matrix, dt: float):
         """
@@ -66,10 +66,11 @@ class Particle:
         # Calculate the matrix A as described in the Crank-Nicolson method
         # as a banded matrix
         off_diag = -0.5j * dt * kinetic_operator.diagonal(1)
-        off_diag_lower = np.concatenate(([0], off_diag))
-        off_diag_upper = np.concatenate((off_diag, [0]))
+        off_diag_lower = np.concatenate((off_diag, [0]))
+        off_diag_upper = np.concatenate(([0], off_diag))
+        #print(potential.diagonal(0))
         diag = np.ones(len(self.spatial_grid)) - 0.5j * dt * (kinetic_operator.diagonal(0) + potential.diagonal(0))
-        A = np.array([off_diag_lower, diag, off_diag_upper])
+        A = np.array([off_diag_upper, diag, off_diag_lower])
 
         # Calculate right hand side of the equation
         u2 = sp.sparse.eye(len(self.spatial_grid), format='csr') + 0.5j * dt * (kinetic_operator + potential)

potential.py
@@ -58,10 +58,11 @@ class InfiniteSquareWell(PotentialBase):
 
 
 class HarmonicOscillator(PotentialBase):
-    def __init__(self, spatial_grid: np.ndarray, center: float, omega: float):
+    def __init__(self, spatial_grid: np.ndarray, center: float, omega: float, mass: float = 1):
         super().__init__(spatial_grid, "Harmonic Oscillator")
         self.omega = omega
         self.center = center
-        self.potential = 0.5 * self.omega ** 2 * np.square(spatial_grid - center)
+        self.potential = 0.5 * self.omega ** 2 * mass * np.square(
+            self.spatial_grid - self.center)
 
 